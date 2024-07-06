ff91ad00083bb714242e641f0264be89605dd60c
User221701 <Olegpoddorogin@mail.ru>
2024-07-06 20:52:26 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,11 @@
+def shifr(n):
+    result = ''
+    for i in range(1,n):
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
+
+
+for i in range(3,21):
+    print(i,'-',shifr(i))
